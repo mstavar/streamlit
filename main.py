9df03afd52d484a5d9a78426a27a7dc21bbcca3c
mstavar <111427961+mstavar@users.noreply.github.com>
2026-01-31 02:11:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ df = load_data()
 # --- PLOT ---
 fig = px.bar(
     df,
-    x='month',
+    x='month', name='Profrrit (%)',
     y=['sales', 'purchases'],
     barmode='group',
     title="Situație financiară lunară",
