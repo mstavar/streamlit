d4512e6cd4844349ab4cd1f4f4aa4e91d98bee5b
mstavar <111427961+mstavar@users.noreply.github.com>
2026-01-31 02:30:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ df = load_data()
 fig = px.bar(
     df,
     x='month', 
-    y=['Vânzări', 'Achiziții']
+    y=['Vânzări', 'Achiziții'],
     barmode='group',
     title="Situație financiară lunară",
     labels={
